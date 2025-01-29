a88ef57dd4d08f33f42381ae64d97b737076543d
danielcruz <danielcscruz@gmail.com>
2025-01-29 17:57:49 -0300

    project set up

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const log = require('fancy-log');
+
+
+
+function styles(){
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+        .on('end', () => log('CSS compressed successfully'));
+}
+
+exports.default = styles;
+exports.watch = function(){
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
